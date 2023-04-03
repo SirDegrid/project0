6631794b7040f4eaa7b53bd2c39efee5f61832dc
Maggie Cameron <maggie.rose.cameron@gmail.com>
2023-04-03 16:46:18 +1000

    some progress adding clicks

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,43 @@
-console.log('omg tic tac toe');
+$(document).ready(function () {
+    
+   $('.box').on("click", function (event) {
+    const markX = [];
+    const markO= [];
+
+    markX.push(event.target.id);
+    console.log(markX);
+    event.target.innerHTML = "X";
+       
+    // markO.push(event.target.id);
+    // console.log(markO);
+    // event.target.innerHTML = "0";
+        
+    
+})})
+
+
+//     ***test alert *** $('#foo').bind('click', function() {
+//         alert('User clicked on "foo."');
+// //       });
+// $('.box').on("click", function () {
+//     alert ('Hello');
+//     }
+//  ****      
+   
+    // add your player mark in selected box 
+    
+    // const markX = "X";
+    // const boxSelection = 
+
+    
+    // const player mark
+    // const box 
+
+ 
+   
+
+
+
+
+//    const playerO =
+//    const playerX = 
