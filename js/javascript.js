13627abb7d7c33976851757b301350f9f3a0eaa3
Maggie Cameron <maggie.rose.cameron@gmail.com>
2023-04-05 12:56:44 +1000

    working game without CSS or readme

--- js/javascript.js
+++ js/javascript.js
@@ -2,26 +2,9 @@ $(document).ready(function () {
     
     const markX = [];
     const markO= [];
-    let count = 0;
-
-    $('.box').on("click", function (event) {
-            
-        count++; 
-        $("#counter").html(+count);
-        $("#counter").hide(); 
-
-if (count %2 === 0) {
-    markX.push(event.target.id);
-    event.target.innerHTML = "X";
-    console.log(markX); 
-
-} else {
-        markO.push(event.target.id);
-        console.log(markO);
-        event.target.innerHTML = "0";
-}
-
-const winningCombinations = [
+    let count = 1;
+    $("#turnIndicator").html("X turn");
+    const winningCombinations = [
         ["1","2","3"],
         ["4","5","6"],
         ["7","8","9"],
@@ -31,20 +14,66 @@ const winningCombinations = [
         ["1","5","9"],
         ["3","5","7"]
             ];
+    
+        $('.box').on("click", function (event) {
+ 
+        count++; 
+        $("#counter").html(count);
+        $("#counter").hide(); 
+
+if (event.target.innerHTML.length === 0) {
+    
+if (count %2 === 0) {    
+    markX.push(event.target.id);
+    event.target.innerHTML = "X";
+    console.log(markX); 
+    $("#turnIndicator").html("O turn"); 
+
+} else {
+        markO.push(event.target.id);
+        event.target.innerHTML = "0";
+        console.log(markO);
+        $("#turnIndicator").html("X turn");
+} 
+} else { 
+    alert('already clicked');
+    (count = (count-1));
+}
+
 const xWins = winningCombinations.some(function(checkForWinner) {
-    return checkForWinner.every(function(why) {
-      return markX.indexOf(why) !=-1
+    return checkForWinner.every(function(xWins) {
+      return markX.indexOf(xWins) !=-1
     })
 })
-    console.log(xWins);
-
 const oWins = winningCombinations.some(function(checkForWinner) {
-    return checkForWinner.every(function(why) {
-        return markO.indexOf(why) !=-1
+    return checkForWinner.every(function(oWins) {
+        return markO.indexOf(oWins) !=-1
         })        
     })
-        console.log(oWins);
+    if (xWins) {alert("X wins - press okay to play again");
+    location.reload();};
+         
+    if (oWins) {alert("O wins - press okay to play again"); 
+    location.reload();};
+
+    if (count === 10) {alert("it's a draw - press okay to play again"); 
+    location.reload();};
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
 }
+
 )});
 
 
