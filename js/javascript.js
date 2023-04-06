42813b5ce6ab96aca8d336e49ca25b12fbe433f9
Maggie Cameron <maggie.rose.cameron@gmail.com>
2023-04-06 10:17:59 +1000

    final game without css or readme

--- js/javascript.js
+++ js/javascript.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     const markX = [];
     const markO= [];
     let count = 1;
-    $("#turnIndicator").html("X turn");
+    $("#turnIndicator").html("Player X, take your turn.");
     const winningCombinations = [
         ["1","2","3"],
         ["4","5","6"],
@@ -14,32 +14,34 @@ $(document).ready(function () {
         ["1","5","9"],
         ["3","5","7"]
             ];
-    
+            
+    $("#xWinner").hide(); 
+    $("#oWinner").hide(); 
+    $("#restartPrompt").hide(); 
+    $("#draw").hide(); 
+    $("#button").hide(); 
+
         $('.box').on("click", function (event) {
- 
         count++; 
         $("#counter").html(count);
         $("#counter").hide(); 
-
 if (event.target.innerHTML.length === 0) {
-    
-if (count %2 === 0) {    
+
+    if (count %2 === 0) {    
     markX.push(event.target.id);
     event.target.innerHTML = "X";
     console.log(markX); 
-    $("#turnIndicator").html("O turn"); 
-
+    $("#turnIndicator").html("Player O, take your turn."); 
 } else {
         markO.push(event.target.id);
         event.target.innerHTML = "0";
         console.log(markO);
-        $("#turnIndicator").html("X turn");
+        $("#turnIndicator").html("Player X, take your turn.");
 } 
 } else { 
     alert('already clicked');
     (count = (count-1));
 }
-
 const xWins = winningCombinations.some(function(checkForWinner) {
     return checkForWinner.every(function(xWins) {
       return markX.indexOf(xWins) !=-1
@@ -50,31 +52,32 @@ const oWins = winningCombinations.some(function(checkForWinner) {
         return markO.indexOf(oWins) !=-1
         })        
     })
-    if (xWins) {alert("X wins - press okay to play again");
-    location.reload();};
-         
-    if (oWins) {alert("O wins - press okay to play again"); 
-    location.reload();};
+    if (xWins) {
+        $("#button").show(); 
+        $("#xWinner").show();
+        $("#restartPrompt").show(); 
+        $("#turnIndicator").hide(); 
+        $('.box').on("click", function (event) {
+        event.target.innerHTML = "";
+        });
+    }
+    else if (oWins) {
+        $("#button").show(); 
+        $("#oWinner").show(); 
+        $("#restartPrompt").show(); 
+        $("#turnIndicator").hide(); 
+        $('.box').on("click", function (event) {
+        event.target.innerHTML = "";
+        $("#xWinner").hide();
+        });
+            
+    }
+    if (count === 10) {
+        $("#button").show(); 
+        $("#draw").show(); 
+        $("#restartPrompt").show(); 
+        $("#turnIndicator").hide(); 
 
-    if (count === 10) {alert("it's a draw - press okay to play again"); 
-    location.reload();};
-
-
-
-
-   
-
-
-
-
-
-
-
-
-
+    }
 }
-
 )});
-
-
-
